bb2c94103b68f63712262c1e21bb06c3cd28835c
EldarLis <eldaarliis@gmail.com>
2020-03-30 17:21:21 +0300

    Add files via upload

---
+++ midtest_task-CoordinateProcessing.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace lab_epam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char check;
+            Console.WriteLine("Input x and y have to be strictly divided by comma , ");
+            Console.WriteLine("Every pair of input have to be on different lines.");
+            Console.WriteLine("Spaces are ignored.");
+            Console.WriteLine("F/f - for file; K/k - for keyboard. ?:");
+            check = char.Parse(Console.ReadLine());
+            string[] words;
+
+            StringBuilder res = new StringBuilder();
+
+            if (check == 'f' || check == 'F')
+            {
+                if (!File.Exists(@"input.txt"))
+                {
+                    Console.WriteLine("File doesn't exist.\nTerminating process");
+                }
+                else
+                {
+                    StreamReader input = new StreamReader("input.txt");
+
+                    string line = input.ReadLine();
+
+                    while (line != null)
+                    {
+
+                        words = line.Split(',');
+                        res.Append("X: " + words[0].Replace(" ", "") + " \tY: " + words[1].Replace(" ", "") + "\n");
+                        line = input.ReadLine();
+                    }
+
+                    Console.WriteLine(res.ToString());
+                }
+            }
+            else if (check == 'k' || check == 'K')
+            {
+                Console.WriteLine("Write lines . Empty line to end");
+                string line = Console.ReadLine();
+                while (line != "")
+                {
+                    words = line.Split(',');
+                    res.Append("X: " + words[0].Replace(" ", "") + " \tY: " + words[1].Replace(" ", "") + "\n");
+                    line = Console.ReadLine();
+                }
+
+                Console.WriteLine(res.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Wrong input.\nProcess terminated!");
+            }
+
+        }
+    }
+}
